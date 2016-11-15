728c4434b838778250a3d8a86e24b6c340c108b8
Sandro Mateo and Tony Fuentes <tony@>
2016-11-15 15:34:27 -0800

    add background images

--- app/src/main/java/com/example/guest/madlibs/StoryActivity.java
+++ app/src/main/java/com/example/guest/madlibs/StoryActivity.java
@@ -17,7 +17,8 @@ public class StoryActivity extends AppCompatActivity {
         String[] myStory = res.getStringArray(R.array.my_story);
         String[] wordArray = intent.getStringArrayExtra("wordArray");
         TextView output = (TextView) findViewById(R.id.textStory);
+//        output.setText("One Day " + wordArray[0] + " and " + wordArray[1] + " were walking through the woods, when suddenly a giant " + wordArray[2] + " appeared. " + wordArray[3] + ", " + wordArray[0] + " cried. The two of them " + wordArray[4] + " as quickly possible, and when they were safe, " + wordArray[0] + " and " + wordArray[1] + " gave each other a giant " + wordArray[5]);
 
-        output.setText(myStory[0] + wordArray[0] + myStory[1] + wordArray[1] + myStory[2] + wordArray[2] + myStory[3] + wordArray[3] + myStory[4] + wordArray[0] + myStory[5] + wordArray[4] + myStory[6] + wordArray[0] + myStory[7] + wordArray[1] + myStory[8] + wordArray[5]);
+        output.setText(myStory[0] + wordArray[0] + myStory[1] + wordArray[1] + myStory[2] + wordArray[2] + myStory[3] + wordArray[3] + myStory[4] + wordArray[0] + myStory[5] + wordArray[4] + myStory[6] + wordArray[0] + myStory[7] + wordArray[1] + myStory[8] + wordArray[5] + myStory[9]);
     }
 }
